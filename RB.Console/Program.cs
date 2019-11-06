ca1decc5ba357b048d4d0a80b77ca2c4dda35df2
Cain Voong <cainux@gmail.com>
2019-11-06 05:07:56 +0000

    Added console app, which reads input from file

---
+++ RB.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using RB.Core;
+using System.IO;
+
+namespace RB.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = "input.txt";
+
+            if (args != null && args.Length > 0)
+            {
+                inputFile = args[0];
+            }
+
+            var input = File.ReadAllLines(inputFile);
+
+            System.Console.WriteLine("--- INPUT ---");
+
+            foreach (var line in input)
+            {
+                System.Console.WriteLine(line);
+            }
+
+            var simulation = new Simulation(input);
+            var result = simulation.Run();
+
+            System.Console.WriteLine("--- OUTPUT ---");
+
+            System.Console.Write(result);
+        }
+    }
+}
