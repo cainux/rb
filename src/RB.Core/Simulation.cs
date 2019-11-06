d2c6f3a17f837dd3079f9d8f0cac82fe299e8f19
Cain Voong <cainux@gmail.com>
2019-11-06 04:27:27 +0000

    Simulation class added. Added test case that matched example in the documentation.

---
+++ src/RB.Core/Simulation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RB.Core
+{
+    public class Simulation
+    {
+        private Planet mars;
+        private IList<Robot> robots;
+
+        public Simulation(string[] input)
+        {
+            var planetLine = input[0];
+            var dimensions = planetLine.Split().Select(x => Convert.ToInt32(x)).ToArray();
+
+            mars = new Planet(dimensions[0], dimensions[1]);
+
+            var i = 1;
+            robots = new List<Robot>();
+
+            while (i < input.Length)
+            {
+                var robotLine1 = input[i].Split();
+
+                var x = Convert.ToInt32(robotLine1[0]);
+                var y = Convert.ToInt32(robotLine1[1]);
+                var orientation = Convert.ToChar(robotLine1[2]);
+                var instructions = input[i + 1];
+
+                var robot = new Robot(x, y, orientation, instructions, mars);
+
+                robots.Add(robot);
+
+                i += 2;
+            }
+        }
+
+        public string Run()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var robot in robots)
+            {
+                robot.Run();
+                sb.AppendLine(robot.Status);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ tests/RB.Tests/SimulationTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using RB.Core;
+using Xunit;
+
+namespace RB.Tests
+{
+    public class SimulationTests
+    {
+        [Fact]
+        public void RBExample()
+        {
+            // Arrange
+            var input = new[]
+            {
+                "5 3",
+                "1 1 E",
+                "RFRFRFRF",
+                "3 2 N",
+                "FRRFLLFFRRFLL",
+                "0 3 W",
+                "LLFFFLFLFL"
+            };
+
+            var simulation = new Simulation(input);
+
+            // Act
+            var actual = simulation.Run();
+
+            // Assert
+            actual.Should().Be(
+@"1 1 E
+3 3 N LOST
+2 3 S
+"
+);
+        }
+    }
+}
