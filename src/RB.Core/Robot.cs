d74437cf1c26f5bcfec884847d50836e5ab87cf9
Cain Voong <cainux@gmail.com>
2019-11-06 01:57:39 +0000

    Basic robot movement

--- src/RB.Core/Robot.cs
+++ src/RB.Core/Robot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RB.Core
 {
@@ -11,6 +12,10 @@ namespace RB.Core
         private Planet planet;
         private bool isLost;
 
+        private Dictionary<char, Action> commandHandlers;
+
+        public string Status => $"{x} {y} {orientation}" + (isLost ? " LOST" : string.Empty);
+
         public Robot(int x, int y, char orientation, string instructions, Planet planet)
         {
             this.x = x;
@@ -18,14 +23,72 @@ namespace RB.Core
             this.orientation = orientation;
             this.instructions = instructions;
             this.planet = planet;
-            this.isLost = false;
+
+            isLost = false;
+
+            commandHandlers = new Dictionary<char, Action>
+            {
+                { 'F', Forward },
+                { 'R', Right },
+                { 'L', Left }
+            };
+        }
+
+        private void Forward()
+        {
+            var nextPosition = MovementHelper.GetNextPosition[orientation](x, y);
+
+            x = nextPosition.Item1;
+            y = nextPosition.Item2;
+        }
+
+        private void Right()
+        {
+            var nextOrientation = MovementHelper.NextRightOrientations[orientation];
+
+            orientation = nextOrientation;
+        }
+
+        private void Left()
+        {
+            var nextOrientation = MovementHelper.NextLeftOrientations[orientation];
+
+            orientation = nextOrientation;
         }
 
         public void Run()
         {
-            throw new NotImplementedException();
+            foreach (var i in instructions)
+            {
+                commandHandlers[i]();
+            }
         }
 
-        public string Status => $"{x} {y} {orientation}" + (isLost ? " LOST" : string.Empty);
+        private static class MovementHelper
+        {
+            public static readonly Dictionary<char, Func<int, int, (int, int)>> GetNextPosition = new Dictionary<char, Func<int, int, (int, int)>>
+            {
+                { 'N', (x, y) => (x, y + 1) },
+                { 'E', (x, y) => (x + 1, y) },
+                { 'S', (x, y) => (x, y - 1) },
+                { 'W', (x, y) => (x - 1, y) }
+            };
+
+            public static readonly Dictionary<char, char> NextRightOrientations = new Dictionary<char, char>
+            {
+                { 'N', 'E' },
+                { 'E', 'S' },
+                { 'S', 'W' },
+                { 'W', 'N' }
+            };
+
+            public static readonly Dictionary<char, char> NextLeftOrientations = new Dictionary<char, char>
+            {
+                { 'N', 'W' },
+                { 'E', 'N' },
+                { 'S', 'E' },
+                { 'W', 'S' }
+            };
+        }
     }
 }
--- tests/RB.Tests/RobotTests.cs
+++ tests/RB.Tests/RobotTests.cs
@@ -7,11 +7,27 @@ namespace RB.Tests
     public class RobotTests
     {
         [Theory]
-        [InlineData(5, 5, 3, 3, 'N', "F", 3, 4, 'N', false)] // Move North
-        [InlineData(5, 5, 3, 3, 'E', "F", 4, 3, 'N', false)] // Move East
-        [InlineData(5, 5, 3, 3, 'S', "F", 3, 2, 'N', false)] // Move South
-        [InlineData(5, 5, 3, 3, 'W', "F", 2, 3, 'N', false)] // Move West
-        public void Moves(
+        // Moving Forward
+        [InlineData(5, 5, 3, 3, 'N', "F", 3, 4, 'N', false)]
+        [InlineData(5, 5, 3, 3, 'E', "F", 4, 3, 'E', false)]
+        [InlineData(5, 5, 3, 3, 'S', "F", 3, 2, 'S', false)]
+        [InlineData(5, 5, 3, 3, 'W', "F", 2, 3, 'W', false)]
+
+        // Rotating Right
+        [InlineData(0, 0, 0, 0, 'N', "R", 0, 0, 'E', false)]
+        [InlineData(0, 0, 0, 0, 'E', "R", 0, 0, 'S', false)]
+        [InlineData(0, 0, 0, 0, 'S', "R", 0, 0, 'W', false)]
+        [InlineData(0, 0, 0, 0, 'W', "R", 0, 0, 'N', false)]
+
+        // Rotating Left
+        [InlineData(0, 0, 0, 0, 'N', "L", 0, 0, 'W', false)]
+        [InlineData(0, 0, 0, 0, 'E', "L", 0, 0, 'N', false)]
+        [InlineData(0, 0, 0, 0, 'S', "L", 0, 0, 'E', false)]
+        [InlineData(0, 0, 0, 0, 'W', "L", 0, 0, 'S', false)]
+
+        // Combination of movements
+        [InlineData(5, 5, 0, 0, 'N', "FRFLFRRFLFF", 3, 1, 'E', false)]
+        public void Movement(
             int planetWidth, int planetHeight,
             int startX, int startY, char startOrientation, string instructions,
             int expectedX, int expectedY, char expectedOrientation, bool expectedLost)
